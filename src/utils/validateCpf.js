99b5a8699d32915911c0f1e0d402f316f0336022
LucasPereira9 <lucas970997@gmail.com>
2024-06-06 15:14:57 -0300

    fix: :hammer: do not allow special characters in the cpf

--- src/utils/validateCpf.js
+++ src/utils/validateCpf.js
@@ -1,6 +1,10 @@
 const { cpf } = require('cpf-cnpj-validator');
 
 const validateCPF = (cpfNumber) => {
+  if (/[^0-9]/.test(cpfNumber)) {
+    return false;
+  }
+
   return cpf.isValid(cpfNumber);
 };
 
